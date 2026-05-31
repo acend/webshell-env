4c24a72c44e29708b33466e109f82da3a8df95ca
Thomas Philipona <thomas@tim-koko.ch>
2026-05-31 10:37:05 +0200

    add more flexiblity to configure kubeconfig

--- build/create_kubeconfig.sh
+++ build/create_kubeconfig.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
-NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
+SA_NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
 CA=$(cat /run/secrets/kubernetes.io/serviceaccount/ca.crt | base64 -w0)
 
-cat << EOF > kubeconfig.yaml
+KUBECONFIG_PATH="${KUBECONFIG_PATH:-${KUBECONFIG:-kubeconfig.yaml}}"
+KUBECONFIG_NAMESPACE="${KUBECONFIG_NAMESPACE:-$SA_NAMESPACE}"
+KUBECONFIG_CLUSTER_NAME="${KUBECONFIG_CLUSTER_NAME:-acend-training-cluster}"
+KUBECONFIG_CONTEXT="${KUBECONFIG_CONTEXT:-acend-training}"
+KUBECONFIG_USERNAME="${KUBECONFIG_USERNAME:-acend-user}"
+
+mkdir -p "$(dirname "$KUBECONFIG_PATH")"
+
+cat << EOF > "$KUBECONFIG_PATH"
 apiVersion: v1
 clusters:
 - cluster:
     certificate-authority-data: ${CA}
     server: https://${CLUSTER_K8S_API_HOST}:6443
-  name: acend-training-cluster
+  name: ${KUBECONFIG_CLUSTER_NAME}
 contexts:
 - context:
-    cluster: acend-training-cluster
-    namespace: ${NAMESPACE}
-    user: acend-user
-  name: acend-training
-current-context: acend-training
+    cluster: ${KUBECONFIG_CLUSTER_NAME}
+    namespace: ${KUBECONFIG_NAMESPACE}
+    user: ${KUBECONFIG_USERNAME}
+  name: ${KUBECONFIG_CONTEXT}
+current-context: ${KUBECONFIG_CONTEXT}
 kind: Config
 preferences: {}
 users:
-- name: acend-user
+- name: ${KUBECONFIG_USERNAME}
   user:
     token: ${TOKEN}
 EOF
